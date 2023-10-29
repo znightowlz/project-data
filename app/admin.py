d342abd9ff35e918fd472fa37b06b35def9fcb2b
asdsahgj <70609084+pondpondpond137@users.noreply.github.com>
2023-10-29 22:33:17 +0700

    Delete app directory

--- app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from app.models import Client, Product
-
-# Register your models here.
-admin.site.register(Client)
-admin.site.register(Product)
--- app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from app import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('signin', views.signin, name='signin'),
-    path('homepage', views.homepage, name='homepage'),
-    path('login', views.login, name='login')
-]
